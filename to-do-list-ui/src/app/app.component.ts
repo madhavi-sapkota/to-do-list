f87813369c1a925eb4de6ba039fa65eafee18e17
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-03-20 14:41:34 -0500

    updated css in to-do-list item

--- to-do-list-ui/src/app/app.component.ts
+++ to-do-list-ui/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do-list-ui';
+
+  todos = [
+    { id: 1, task: 'first todo item', isactive: true },
+    { id: 2, task: 'second todo item', isactive: true },
+    { id: 3, task: 'third todo item', isactive: true },
+    { id: 4, task: 'forth todo item', isactive: true },
+  ];
 }
