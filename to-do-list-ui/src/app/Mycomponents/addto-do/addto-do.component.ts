ec94916702692cc8777ae7bb72441739383198dd
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-03-21 14:32:45 -0500

    delete and update

--- to-do-list-ui/src/app/Mycomponents/addto-do/addto-do.component.ts
+++ to-do-list-ui/src/app/Mycomponents/addto-do/addto-do.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-addto-do',
   templateUrl: './addto-do.component.html',
-  styleUrls: ['./addto-do.component.css']
+  styleUrls: ['./addto-do.component.css'],
 })
 export class AddtoDoComponent {
-
+  taskName = '';
+  @Output() newItemEvent = new EventEmitter();
+  onClick() {
+    this.newItemEvent.emit(this.taskName);
+    this.taskName = '';
+  }
 }
--- to-do-list-ui/src/app/app.component.ts
+++ to-do-list-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list-ui';
+  newText = '';
 
   todos = [
-    { id: 1, task: 'first todo item', isactive: true },
-    { id: 2, task: 'second todo item', isactive: true },
-    { id: 3, task: 'third todo item', isactive: true },
-    { id: 4, task: 'forth todo item', isactive: true },
+    { id: uuidv4(), task: 'first todo item', isActive: true },
+    { id: uuidv4(), task: 'second todo item', isActive: true },
+    { id: uuidv4(), task: 'third todo item', isActive: true },
+    { id: uuidv4(), task: 'forth todo item', isActive: true },
   ];
+  addNewTodo(newTodo: any) {
+    let todoNewItem = {
+      id: uuidv4(),
+      task: newTodo,
+      isActive: true,
+    };
+    this.todos.push(todoNewItem);
+  }
+
+  deleteItem(id: string) {
+    this.todos = this.todos.filter((item) => item.id !== id);
+  }
+
+  updateItem(id: string, target: any) {
+    console.log(id, target.value);
+    this.newText = target.value;
+    console.log(this.newText);
+  }
 }
