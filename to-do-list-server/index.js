8abc1b48d7ad2648451a69c18d9b84abfb1514a8
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-04-16 16:18:52 -0500

    mongodb completed for task

--- to-do-list-server/index.js
+++ to-do-list-server/index.js
@@ -4,38 +4,24 @@ const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
-
-let todos = [
-  { id: "1", task: "first todo item", dueDate: new Date(), isActive: true },
-  {
-    id: uuidv4(),
-    task: "second todo item",
-    dueDate: new Date(),
-    isActive: false,
-  },
-  {
-    id: uuidv4(),
-    task: "third todo item",
-    dueDate: new Date(),
-    isActive: false,
-  },
-  {
-    id: uuidv4(),
-    task: "forth todo item",
-    dueDate: new Date(),
-    isActive: true,
-  },
-];
+const {
+  addTask,
+  getAllTasks,
+  updateTask,
+  deleteTask,
+  updateIsActive,
+} = require("./respositories/task-repository");
 
 app.get("/", function (req, res) {
   res.send("Hello Worlds");
 });
 
-app.get("/to-do-items", function (req, res) {
-  res.send(todos);
+app.get("/to-do-items", async function (req, res) {
+  tasks = await getAllTasks();
+  res.send(tasks);
 });
 
-app.post("/add-item", function (req, res) {
+app.post("/add-item", async function (req, res) {
   let taskName = req.body.taskName;
   let dueDate = req.body.dueDate;
   if (!taskName) {
@@ -48,37 +34,33 @@ app.post("/add-item", function (req, res) {
     dueDate: dueDate,
     isActive: true,
   };
-  todos.unshift(todoNewItem);
+  await addTask(todoNewItem);
   res.send(todoNewItem);
 });
 
-app.post("/update-item", function (req, res) {
+app.post("/update-item", async function (req, res) {
   let newTaskName = req.body.newTaskName;
   let id = req.body.id;
   let newDueDate = req.body.newDueDate;
-  let index = todos.findIndex((item) => item.id == id);
-  todos[index].task = newTaskName;
-  todos[index].dueDate = newDueDate;
+  await updateTask(id, newTaskName, newDueDate);
   res.status(200).send();
 });
 
-app.post("/delete-item", function (req, res) {
+app.post("/delete-item", async function (req, res) {
   let id = req.body.id;
-  todos = todos.filter((item) => item.id !== id);
+  await deleteTask(id);
   res.status(200).send();
 });
 
-app.post("/mark-complete", function (req, res) {
+app.post("/mark-complete", async function (req, res) {
   let id = req.body.id;
-  let index = todos.findIndex((item) => item.id === id);
-  todos[index].isActive = false;
+  await updateIsActive(id, false);
   res.status(200).send();
 });
 
-app.post("/mark-active", function (req, res) {
+app.post("/mark-active", async function (req, res) {
   let id = req.body.id;
-  let index = todos.findIndex((item) => item.id === id);
-  todos[index].isActive = true;
+  await updateIsActive(id, true);
   res.status(200).send();
 });
 
---
+++ to-do-list-server/respositories/task-repository.js
@@ -0,0 +1,70 @@
+const { MongoClient } = require("mongodb");
+var url = "mongodb://127.0.0.1:27017/tasks-to-do";
+const client = new MongoClient(url);
+
+async function getAllTasks() {
+  let connection = await client.connect();
+  let tasks = await connection.db().collection("tasks").find({}).toArray();
+  return tasks;
+}
+
+async function addTask(task) {
+  let connection = await client.connect();
+  return await connection.db().collection("tasks").insertOne(task);
+}
+
+async function updateTask(id, taskName, dueDate) {
+  let connection = await client.connect();
+  return await connection
+    .db()
+    .collection("tasks")
+    .updateOne({ id: id }, { $set: { task: taskName, dueDate: dueDate } });
+}
+
+async function deleteTask(id) {
+  let connection = await client.connect();
+  return await connection.db().collection("tasks").deleteOne({ id: id });
+}
+
+async function updateIsActive(id, isActive) {
+  let connection = await client.connect();
+  return await connection
+    .db()
+    .collection("tasks")
+    .updateOne({ id: id }, { $set: { isActive: isActive } });
+}
+
+module.exports = {
+  addTask,
+  getAllTasks,
+  updateTask,
+  deleteTask,
+  updateIsActive,
+};
+// then way of doing it
+// function getAllTasks() {
+//   client.connect().then((connection) => {
+//     connection
+//       .db()
+//       .collection("tasks")
+//       .find({})
+//       .toArray()
+//       .then((tasks) => {
+//         return tasks;
+//       });
+//   });
+// }
+
+// client.connect().then((connection) => {
+// connection.db().collection("testDbCollection").insertOne({
+//   name: "Helllo",
+// });
+// let result = connection
+//   .db()
+//   .collection("testDbCollectionrrrr")
+//   .find({})
+//   .toArray();
+// result.then((val) => {
+//   console.log(val);
+// });
+// });
